d7336990af4c67e6c021de48e1be54f5bb92c84d
poonamalai <85272623+poonamalai@users.noreply.github.com>
2021-06-03 14:01:03 +0530

    Update Plane.java

--- src/main/java/com/digite/kata/eg2/Plane.java
+++ src/main/java/com/digite/kata/eg2/Plane.java
@@ -1,6 +1,6 @@
 package com.digite.kata.eg2;
 
-public class Plane extends Vehicle {
+public class Plane extends FlyingVehicle {
     // A plane can reverse engine gear while moving forward, no problem here
 }
 
